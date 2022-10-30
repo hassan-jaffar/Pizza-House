bd7f19e869c13b708349d18a6308ff00fe8fcbcd
Jaffar <hassanjaffar.sh@gmail.com>
2022-10-30 22:33:21 +0500

    Item Detail Added in Menu Modal

--- Restaurant-App/src/components/Items.js
+++ Restaurant-App/src/components/Items.js
@@ -75,7 +75,7 @@ function Items({ items, categorys }) {
               ) : (
                 <></>
               )}
-              <hr/>
+              <hr />
               {items.Price !== "undefined" ? (
                 <h5 className="itemprice">$ {items.Price}</h5>
               ) : (
@@ -99,23 +99,92 @@ function Items({ items, categorys }) {
           <Modal.Title>{items.Title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h5> ${items.Price}</h5>
-          <p>{items.Title}</p>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Quantity</Form.Label>
-              <Form.Control
-                type="number"
-                value={quantity}
-                onChange={(e) => {
-                  setQuantity(e.target.value);
-                }}
-                required
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                className="detailpageimg br"
+                src="https://www.cheezious.com/api/image?url=https%3A%2F%2Fem-cdn.eatmubarak.pk%2Frestaurant_new%2F%2F%2Fdish%2F16027791221858590131.jpg&w=1920&q=100"
+                alt=""
               />
-            </Form.Group>
-          </Form>
+              <h4 className="boldtext redclr mt-5">Product Description</h4>
+              <p>4 Pcs Stuffed Calzone Chunks Served with Sauce & Fries</p>
+            </div>
+            <div className="col-md-6">
+              <h3 className="boldtext redclr mb-5">Item Name</h3>
+              <p>
+                <i className="fa-solid fa-clock me-2 redclr"></i>ETA: 45
+                minute(s)
+              </p>
+              <p>
+                <i className="fa-solid fa-location-pin me-3 redclr"></i>Airline
+                Society, Lahore
+              </p>
+              <p>
+                <i className="fa-solid fa-bus me-2 redclr"></i>Delivered by
+                Pizza House
+              </p>
+              <h5 className="boldtext my-5 redclr">Price</h5>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Quantity</Form.Label>
+                  <Form.Control
+                    type="number"
+                    value={quantity}
+                    onChange={(e) => {
+                      setQuantity(e.target.value);
+                    }}
+                    required
+                  />
+                </Form.Group>
+              </Form>
+              <button className="btn addtocartbtn w-100">Add to Cart</button>
+            </div>
+          </div>
+          {/* <div className="row">
+            <div className="col-md-2">
+              <h4>
+                <i className="fa-solid fa-money-bill redclr"></i>
+              </h4>
+            </div>
+            <div className="col-md-10">
+              <p className="boldtext redclr">Cash On Delivery</p>
+              <p>Pay once you get the order</p>
+            </div>
+          </div>
+          <hr /> */}
+          {/* <div className="row">
+            <div className="col-md-2">
+              <h4>
+                <i className="fa-solid fa-truck redclr"></i>
+              </h4>
+            </div>
+            <div className="col-md-10">
+              <p className="boldtext redclr">Quick Delivery</p>
+              <p>Our ETA is 45 minute(s)</p>
+            </div>
+          </div>
+
+          <hr />
+          <div className="row">
+            <div className="col-md-2">
+              <h4>
+                <i className="fa-solid fa-headset redclr"></i>
+              </h4>
+            </div>
+            <div className="col-md-10">
+              <p className="boldtext redclr">Customer Support</p>
+              <p>Contact us by email or phone</p>
+            </div>
+          </div> */}
+          {/* <div className="row">
+            <div className="col-md-12">
+            <h5> ${items.Price}</h5>
+          <p>{items.Title}</p>
+
+            </div>
+          </div> */}
         </Modal.Body>
-        <Modal.Footer>
+        {/* <Modal.Footer>
           <Button
             variant="outline-dark"
             size="lg"
@@ -124,7 +193,7 @@ function Items({ items, categorys }) {
           >
             Add to Cart
           </Button>
-        </Modal.Footer>
+        </Modal.Footer> */}
       </Modal>
 
       {/* <ToastContainer /> */}
